cf5729871715d4a2598d2a43406f9f048a3f7409
Alfonso Contreras <1923010051@untels.edu.pe>
2022-05-09 11:13:23 -0500

    Pregunta01 - listo

---
+++ Preg01.java
@@ -0,0 +1,45 @@
+packagepackage com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alfonso
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+      
+        Scanner dato=new Scanner(System.in);
+        System.out.println("Ingrese un numero de entrada:");
+        double numero=dato.nextDouble();
+        double resultado=0.25-(1/(numero+4));
+        System.out.println("El resultado de la sumatoria es:");
+        System.out.println(resultado);
+        
+    }
+       
+    
+} com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alfonso
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+      
+        Scanner dato=new Scanner(System.in);
+        System.out.println("Ingrese un numero de entrada:");
+        double numero=dato.nextDouble();
+        double resultado=0.25-(1/(numero+4));
+        System.out.println("El resultado de la sumatoria es:");
+        System.out.println(resultado);
+        
+    }
+       
+    
+}
